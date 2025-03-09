c1413a177d4de0b6b61502ca5b0bcd12f7b737a1
Andrii Pesliak <apesliak@piraeusbank.ua>
2025-03-09 20:32:09 +0200

    Fix password encryption module.

--- adapter/out/password-encryption/src/main/java/com/task/management/password/encryption/BCryptPasswordEncryptorAdapter.java
+++ adapter/out/password-encryption/src/main/java/com/task/management/password/encryption/BCryptPasswordEncryptorAdapter.java
@@ -3,13 +3,15 @@ package com.task.management.password.encryption;
 import com.task.management.domain.common.annotation.AppComponent;
 import com.task.management.domain.common.validation.Validation;
 import com.task.management.domain.iam.port.out.EncryptPasswordPort;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @AppComponent
-@RequiredArgsConstructor
 public class BCryptPasswordEncryptorAdapter implements EncryptPasswordPort {
     private final PasswordEncoder passwordEncoder;
+
+    public BCryptPasswordEncryptorAdapter(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public String encrypt(char[] password) {
