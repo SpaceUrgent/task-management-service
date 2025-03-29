d8ed640883b60ea230fe5e2cb2d5fbbc609daecd
Andrii Pesliak <apesliak@piraeusbank.ua>
2025-03-29 14:19:16 +0200

    Configure jpa entity scan for spring app.

--- application/spring-application/src/main/java/com/task/management/spring/configuration/BeanConfiguration.java
+++ application/spring-application/src/main/java/com/task/management/spring/configuration/BeanConfiguration.java
@@ -1,6 +1,7 @@
 package com.task.management.spring.configuration;
 
 import com.task.management.domain.common.annotation.AppComponent;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
         "com.task.management.password",
         "com.task.management.persistence"
 }, includeFilters = @ComponentScan.Filter(AppComponent.class))
+@EntityScan(basePackages = "com.task.management.persistence.jpa.entity")
 public class BeanConfiguration {
 
 //    @Bean
