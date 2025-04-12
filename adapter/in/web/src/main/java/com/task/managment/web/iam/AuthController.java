f6d1d5bfc1da8685f9e1f5c9916da68acbe6d603
Andrii Pesliak <apesliak@Andriis-MacBook-Pro.local>
2025-04-12 15:20:36 +0300

    Configure session management for web adapter.

---
+++ adapter/in/web/src/main/java/com/task/managment/web/iam/AuthController.java
@@ -0,0 +1,45 @@
+package com.task.managment.web.iam;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final SecurityContextRepository securityContextRepository;
+    private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder.getContextHolderStrategy();
+
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public void login(LoginRequest loginRequest,
+                      HttpServletRequest servletRequest,
+                      HttpServletResponse servletResponse) {
+        UsernamePasswordAuthenticationToken token = UsernamePasswordAuthenticationToken
+                .unauthenticated(
+                        loginRequest.getEmail(), loginRequest.getPassword()
+                );
+        var authentication = authenticationManager.authenticate(token);
+        var context = securityContextHolderStrategy.createEmptyContext();
+
+        context.setAuthentication(authentication); //set context application from authentication
+        securityContextHolderStrategy.setContext(context);
+
+        securityContextRepository.saveContext(context, servletRequest, servletResponse);
+    }
+
+    @Data
+    public static class LoginRequest {
+        private String email;
+        private String password;
+    }
+}
