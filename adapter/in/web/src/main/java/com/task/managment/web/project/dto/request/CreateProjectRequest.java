8a7525734f0744d164d3f00ae582e846e2d6c3c3
Andrii Pesliak <andrey.pslk@gmail.com>
2025-04-15 01:47:09 +0300

    Remove validation for create project description request body property.

--- adapter/in/web/src/main/java/com/task/managment/web/project/dto/request/CreateProjectRequest.java
+++ adapter/in/web/src/main/java/com/task/managment/web/project/dto/request/CreateProjectRequest.java
@@ -7,6 +7,5 @@ import lombok.Data;
 public class CreateProjectRequest {
     @NotBlank(message = "Title is required")
     private String title;
-    @NotBlank(message = "Description is required")
     private String description;
 }
