ec765720ef659b5aff7dac41214c11ee46f4e2df
Andrii Pesliak <andrey.pslk@gmail.com>
2025-05-07 23:59:48 +0300

    Add static factory method for task priority.

--- domain/src/main/java/com/task/management/domain/project/model/objectvalue/TaskPriority.java
+++ domain/src/main/java/com/task/management/domain/project/model/objectvalue/TaskPriority.java
@@ -1,5 +1,11 @@
 package com.task.management.domain.project.model.objectvalue;
 
+import com.task.management.domain.common.validation.ValidationException;
+
+import java.util.Arrays;
+
+import static com.task.management.domain.common.validation.Validation.parameterRequired;
+
 public enum TaskPriority {
     LOWEST(0),
     LOW(1),
@@ -15,5 +21,13 @@ public enum TaskPriority {
 
     public Integer order() {
         return order;
+    }
+
+    public static TaskPriority withOrder(Integer order) {
+        parameterRequired(order, "Task priority order");
+        return Arrays.stream(TaskPriority.values())
+                .filter(priority -> order.equals(priority.order))
+                .findFirst()
+                .orElseThrow(() -> new ValidationException("Unknown task priority order value - %d".formatted(order)));
     }
 }
