4688ed335311515890b47d498154c3eb3c928112
Andrii Pesliak <andrey.pslk@gmail.com>
2025-05-15 21:39:14 +0300

    Add task priorities list to project details projection.

--- application/src/main/java/com/task/management/application/project/projection/ProjectDetails.java
+++ application/src/main/java/com/task/management/application/project/projection/ProjectDetails.java
@@ -1,6 +1,7 @@
 package com.task.management.application.project.projection;
 
 import com.task.management.domain.project.model.objectvalue.ProjectId;
+import com.task.management.domain.project.model.objectvalue.TaskPriority;
 import com.task.management.domain.project.model.objectvalue.TaskStatus;
 import lombok.Builder;
 
@@ -18,16 +19,29 @@ public record ProjectDetails(
         String description,
         MemberView owner,
         List<TaskStatus> taskStatuses,
+        List<TaskPriority> taskPriorities,
         Set<MemberView> members
 ) {
 
     @Builder
+    public ProjectDetails(ProjectId id,
+                          Instant createdAt,
+                          Instant updatedAt,
+                          String title,
+                          String description,
+                          MemberView owner,
+                          List<TaskStatus> taskStatuses,
+                          Set<MemberView> members) {
+        this(id, createdAt, updatedAt, title, description, owner, taskStatuses, TaskPriority.orderedList(), members);
+    }
+
     public ProjectDetails {
         parameterRequired(id, "Project id");
         parameterRequired(createdAt, "Created time");
         notBlank(title, "Title");
         parameterRequired(owner, "Owner");
         notEmpty(taskStatuses, "Task status set");
+        notEmpty(taskPriorities, "Task priorities");
         notEmpty(members, "Members");
     }
 }
