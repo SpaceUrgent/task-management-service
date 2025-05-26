12536220d327f70884eb3eb83607f893502abc9a
Andrii Pesliak <andrey.pslk@gmail.com>
2025-05-26 22:18:03 +0300

    Domain: Add update user methods

--- domain/src/main/java/com/task/management/domain/iam/model/User.java
+++ domain/src/main/java/com/task/management/domain/iam/model/User.java
@@ -2,8 +2,10 @@ package com.task.management.domain.iam.model;
 
 import com.task.management.domain.common.model.objectvalue.Email;
 import com.task.management.domain.common.model.objectvalue.UserId;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Setter;
 
 import java.time.Instant;
 
@@ -13,10 +15,11 @@ import static com.task.management.domain.common.validation.Validation.parameterR
 
 
 @Data
+@Setter(value = AccessLevel.PROTECTED)
 public class User {
     private final UserId id;
     private final Instant createdAt;
-    private final Instant updatedAt;
+    private Instant updatedAt;
     private Email email;
     private String firstName;
     private String lastName;
@@ -34,8 +37,35 @@ public class User {
         this.createdAt = parameterRequired(createdAt, "Created at value");
         this.updatedAt = updatedAt;
         this.email = emailRequired(email);
-        this.firstName = notBlank(firstName, "First name");
-        this.lastName = notBlank(lastName, "Last name");
-        this.encryptedPassword = notBlank(encryptedPassword, "Encrypted password");
+        this.firstName = firstNameRequired(firstName);
+        this.lastName = lastNameRequired(lastName);
+        this.encryptedPassword = encryptedPasswordRequired(encryptedPassword);
+    }
+
+    public void updateName(String firstName, String lastName) {
+        recordUpdatedTime();
+        this.firstName = firstNameRequired(firstName);
+        this.lastName = lastNameRequired(lastName);
+    }
+
+    public void updatePassword(String encryptedPassword) {
+        recordUpdatedTime();
+        this.encryptedPassword = encryptedPasswordRequired(encryptedPassword);
+    }
+
+    private void recordUpdatedTime() {
+        this.updatedAt = Instant.now();
+    }
+
+    private static String firstNameRequired(String firstName) {
+        return notBlank(firstName, "First name");
+    }
+
+    private static String lastNameRequired(String lastName) {
+        return notBlank(lastName, "Last name");
+    }
+
+    private static String encryptedPasswordRequired(String encryptedPassword) {
+        return notBlank(encryptedPassword, "Encrypted password");
     }
 }
