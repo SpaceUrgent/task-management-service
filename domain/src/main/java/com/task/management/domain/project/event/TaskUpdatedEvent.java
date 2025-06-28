2e36a3c1d4b182919621377e8c8b9289470fc837
Andrii Pesliak <andrey.pslk@gmail.com>
2025-06-28 23:01:47 +0300

    Domain: Remove entity id property from abstract domain event.

--- domain/src/main/java/com/task/management/domain/project/event/TaskUpdatedEvent.java
+++ domain/src/main/java/com/task/management/domain/project/event/TaskUpdatedEvent.java
@@ -3,6 +3,7 @@ package com.task.management.domain.project.event;
 import com.task.management.domain.shared.event.AbstractDomainEvent;
 import com.task.management.domain.shared.model.objectvalue.UserId;
 import com.task.management.domain.shared.model.objectvalue.TaskId;
+import com.task.management.domain.shared.validation.Validation;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
@@ -10,7 +11,8 @@ import lombok.ToString;
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @Getter
-public abstract class TaskUpdatedEvent<T> extends AbstractDomainEvent<TaskId> {
+public abstract class TaskUpdatedEvent<T> extends AbstractDomainEvent {
+    private final TaskId taskId;
     private final UserId actorId;
     private final T initialValue;
     private final T newValue;
@@ -19,10 +21,10 @@ public abstract class TaskUpdatedEvent<T> extends AbstractDomainEvent<TaskId> {
                                UserId actorId,
                                T initialValue,
                                T newValue) {
-        super(taskId);
+        super();
+        this.taskId = Validation.parameterRequired(taskId, "Task id");
         this.actorId = actorId;
         this.initialValue = initialValue;
         this.newValue = newValue;
-
     }
 }
--- domain/src/main/java/com/task/management/domain/shared/event/AbstractDomainEvent.java
+++ domain/src/main/java/com/task/management/domain/shared/event/AbstractDomainEvent.java
@@ -1,19 +1,14 @@
 package com.task.management.domain.shared.event;
 
-import com.task.management.domain.shared.model.objectvalue.UserId;
 import lombok.Data;
 
 import java.time.Instant;
 
-import static com.task.management.domain.shared.validation.Validation.parameterRequired;
-
 @Data
-public abstract class AbstractDomainEvent<EntityId> implements DomainEvent {
+public abstract class AbstractDomainEvent implements DomainEvent {
     protected final Instant occurredAt;
-    protected final EntityId entityId;
 
-    public AbstractDomainEvent(EntityId entityId) {
+    public AbstractDomainEvent() {
         this.occurredAt = Instant.now();
-        this.entityId = parameterRequired(entityId, "Entity id");
     }
 }
